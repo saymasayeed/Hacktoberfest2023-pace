2b1a17eab32cbbb14946f299bfb7292e532e1098
saymasayeed <54811316+saymasayeed@users.noreply.github.com>
2023-10-23 13:43:23 +0530

    Create withoutformatsp.c
    C program to print character without using format specifiers. 
    Explanation: In this program, we are printing the value of 0x41, 0x42, 0x43, 0x44 these are the ASCII Codes of A, B, C, D

---
+++ withoutformatsp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\x41");
+    printf("\n");
+
+    printf("\x42");
+    printf("\n");
+
+    printf("\x43");
+    printf("\n");
+
+    printf("\x44");
+    printf("\n");
+
+    return 0;
+}
